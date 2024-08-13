908d9254f49a9afb95d4f819588ff2dd9daaee47
Sthepen_EA <sthepen.espinoza29@gmail.com>
2024-08-13 10:41:01 -0500

    first branch commit

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [],
+  imports: [ToolBarComponent],
   templateUrl: './main-view.component.html',
-  styleUrl: './main-view.component.css'
+  styleUrl: './main-view.component.css',
 })
-export class MainViewComponent {
-
-}
+export class MainViewComponent {}
